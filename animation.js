6e25058c47aefa8a63df03ad3ea6b51a6bad5591
松井晴也 <>
2022-02-06 19:52:23 +0900

    toppageボタンを実装

--- animation.js
+++ animation.js
@@ -1,5 +1,22 @@
-$ (function() {
+$(function() {
     $(".dropdown-toggle").click (function() {
         $(".dropdown-menu").slideToggle();
+    });
+});
+
+$(function() {
+    const pagetop = $('.page_top');
+     pagetop.hide();
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 80) {
+            pagetop.fadeIn(300);
+        }else {
+            pagetop.fadeOut(300);
+        }
+    });
+
+    pagetop.click(function() {
+        $('body, html').animate({ scrollTop:0 }, 500);
+         return false;
     });
 });
